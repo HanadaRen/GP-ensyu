3ebf06aefa53483f9843a00012a93ac19a1357cc
STD\s20217037 <20217037-hanadaren@gmail.ac.jp>
2022-11-08 14:58:51 +0900

    メインメニュー、検索画面、登録画面を追加したよ。

---
+++ Gp_app/Gp_app/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gp_app
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+
+        }
+        private void Form1_Load(object sender, EventArgs e)//フォーム画面
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)//登録画面移動ボタン
+        {
+            Form2 f2 = new Form2();//Form2をf2と定義
+            f2.Show();//Form2を呼び出す
+            this.Visible = false;//Form1を非表示
+        }
+
+        private void button2_Click(object sender, EventArgs e)//検索画面ボタン
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)//終了ボタン
+        {
+            Application.Exit();//終了
+        }
+    }
+}
---
+++ Gp_app/Gp_app/Form2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gp_app
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        private void Form2_Load(object sender, EventArgs e)//フォーム画面
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)//戻るボタン
+        {
+            Form1 f1 = new Form1();//Form1をf1と定義
+            f1.Visible = true;//Form1を表示
+            this.Close();//Form2を閉じる
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)//学籍番号
+        {
+
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)//メルアド
+        {
+
+        }
+        private void textBox3_TextChanged(object sender, EventArgs e)//名前
+        {
+
+        }
+    }
+}

d4131db7c1453ad7cc5ece00be6dfa4cd98ff8ce
STD\s20217037 <20217037-hanadaren@gmail.ac.jp>
2022-11-16 14:44:12 +0900

    テストデータベース完成

--- Gp_app/Gp_app/Form1.Designer.cs
+++ Gp_app/Gp_app/Form1.Designer.cs
@@ -35,6 +35,20 @@ namespace Gp_app
             this.label2 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -89,11 +103,138 @@ namespace Gp_app
             this.textBox2.Size = new System.Drawing.Size(100, 22);
             this.textBox2.TabIndex = 5;
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(235, 111);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(166, 64);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "データ読み込み";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(424, 111);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(364, 97);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(235, 228);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(166, 64);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "データ修正";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(235, 341);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(166, 64);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "データ削除";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(424, 292);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 11;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(530, 292);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 12;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(424, 250);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 22);
+            this.textBox5.TabIndex = 13;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(421, 232);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 15);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "検索CD";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(527, 274);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 15);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "price";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(421, 274);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(89, 15);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "productname";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(424, 383);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 22);
+            this.textBox6.TabIndex = 17;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(421, 365);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(57, 15);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "検索CD";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(30, 110);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(180, 65);
+            this.button6.TabIndex = 19;
+            this.button6.Text = "テーブル削除";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label2);
@@ -102,6 +243,7 @@ namespace Gp_app
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -115,5 +257,18 @@ namespace Gp_app
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button6;
     }
 }
--- Gp_app/Gp_app/Form1.cs
+++ Gp_app/Gp_app/Form1.cs
@@ -19,14 +19,14 @@ namespace Gp_app
         }
 
         private void button1_Click(object sender, EventArgs e)
-        {
+        {   //コネクションを開いてテーブル作成をして閉じる
             using(var con = new SQLiteConnection("Data Source=test.db"))
             {
                 con.Open();
                 using (SQLiteCommand command = con.CreateCommand())
                 {
                     command.CommandText =
-                        "create table t_product(CD INTEGER PRYMARY KEY AUTOINCREMENT, productname TEXT,price INTEGER)";
+                        "create table t_product(CD INTEGER PRIMARY KEY AUTOINCREMENT, productname TEXT,price INTEGER)";
                     command.ExecuteNonQuery();
                 }
                 con.Close();
@@ -41,18 +41,91 @@ namespace Gp_app
                 using(SQLiteTransaction trans = con.BeginTransaction())
                 {
                     SQLiteCommand cmd = con.CreateCommand();
-
-                    cmd.CommandText = "INCERT INTO t_product (productname, price) VALUES (@Product, @Price)";
-
+                    //インサート
+                    cmd.CommandText = "INSERT INTO t_product (productname, price) VALUES (@Product, @Price)";
+                    //パラメータセット
                     cmd.Parameters.Add("Product", System.Data.DbType.String);
                     cmd.Parameters.Add("Price",  System.Data.DbType.Int64);
-
+                    //データ追加
                     cmd.Parameters["Product"].Value = textBox1.Text;
                     cmd.Parameters["Price"].Value = int.Parse(textBox2.Text);
                     cmd.ExecuteNonQuery();
-
+                    //コミット
                     trans.Commit();
                 }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            using (SQLiteConnection con = new SQLiteConnection("Data Source=test.db"))
+            {   //DataTableを作成します。
+                var dataTable = new DataTable();
+                //SQLの実行
+                var adapter = new SQLiteDataAdapter("SELECT * FROM t_product", con);
+                adapter.Fill(dataTable);
+                dataGridView1.DataSource = dataTable;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            using(SQLiteConnection con = new SQLiteConnection("Data Source=test.db"))
+            {
+                con.Open();
+                using(SQLiteTransaction trans = con.BeginTransaction())
+                {
+                    SQLiteCommand cmd = con.CreateCommand();
+                    //インサート
+                    cmd.CommandText = "UPDATE t_product set productname = @Product, price = @Price WHERE CD = @Cd";
+                    //パラメータリセット
+                    cmd.Parameters.Add("Product", System.Data.DbType.String);
+                    cmd.Parameters.Add("Price", System.Data.DbType.Int64);
+                    cmd.Parameters.Add("Cd", System.Data.DbType.Int64);
+                    //データ追加
+                    cmd.Parameters["Product"].Value = textBox3.Text;
+                    cmd.Parameters["Price"].Value = int.Parse(textBox4.Text);
+                    cmd.Parameters["Cd"].Value = int.Parse(textBox5.Text);
+                    cmd.ExecuteNonQuery();
+                    //コミット
+                    trans.Commit();
+                }
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            using(SQLiteConnection con = new SQLiteConnection("Data Source=test.db"))
+            {
+                con.Open();
+                using(SQLiteTransaction trans = con.BeginTransaction())
+                {
+                    SQLiteCommand cmd = con.CreateCommand();
+                    //インサート
+                    cmd.CommandText = "DELETE FROM t_product WHERE CD = @Cd;";
+                    //パラメータセット
+                    cmd.Parameters.Add("Cd", System.Data.DbType.Int64);
+                    //データ削除
+                    cmd.Parameters["Cd"].Value = int.Parse(textBox6.Text);
+                    cmd.ExecuteNonQuery();
+                    //コミット
+                    trans.Commit();
+                }
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {   //コネクションを開いてテーブル削除をして閉じる
+            using (var con = new SQLiteConnection("Data Source=test.db"))
+            {
+                con.Open();
+                using (SQLiteCommand command = con.CreateCommand())
+                {
+                    command.CommandText =
+                        "drop table t_product";
+                    command.ExecuteNonQuery();
+                }
+                con.Close();
             }
         }
     }
